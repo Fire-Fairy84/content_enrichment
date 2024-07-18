e1af7743da1a77f7bcffe3e479b36568641d12f4
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-07-18 14:22:26 +0200

    try exception

--- src/CreatePDF.py
+++ src/CreatePDF.py
@@ -7,22 +7,28 @@ class SaveFile:
     def __init__(self, text):
         self.text = text
     def saveAsPdf(self, fileName):
-        c = canvas.Canvas(fileName, pagesize=letter)
-        width, height = letter
-        x = 72
-        y = height - 72
-        c.drawString(x, y, self.text)
-        c.save()
+        try:
+            c = canvas.Canvas(fileName, pagesize=letter)
+            width, height = letter
+            x = 72
+            y = height - 72
+            c.drawString(x, y, self.text)
+            c.save()
+        except Exception as e:
+            return f"An expected error was found: {e}"
     def saveAsTxt(self, fileName):
-        with open(fileName, 'w') as file:
-            file.write(self.text)
+        try:
+            with open(fileName, 'w') as file:
+                file.write(self.text)
+        except Exception as e:
+            return f"An expected error was found: {e}"
 
 
 text = "Aquí irá el texto enriquecido y traducido."
 save_file = SaveFile(text)
 
-fileNamePdf = "file1.pdf"
+fileNamePdf = "file.pdf"
 save_file.saveAsPdf(fileNamePdf)
 
-fileNameTxt = "file1.txt"
+fileNameTxt = "file.txt"
 save_file.saveAsTxt(fileNameTxt)
